a20e3897864e9fb81e35aafcd8b1bbbc262a8ca4
OneNotePerPage <danijel.james+onenoteperpage@gmail.com>
2024-05-22 11:43:59 +1000

    move controllers, add healthcheck, add pgsql support

---
+++ ActivusX.API/Controllers/System/HealthController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActivusX.API.Controllers.System
+{
+    [Route("api/System/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        private const string ApiKeyHeaderName = "X-API-KEY";
+        private const string ApiKey = "your-secure-api-key"; // Replace with your actual API key
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            if (!Request.Headers.TryGetValue(ApiKeyHeaderName, out var extractedApiKey))
+            {
+                return Unauthorized(new { Message = "API Key was not provided." });
+            }
+
+            if (!ApiKey.Equals(extractedApiKey))
+            {
+                return Unauthorized(new { Message = "Unauthorized client." });
+            }
+
+            return Ok(new { Message = "Hello, World!" });
+        }
+    }
+}
