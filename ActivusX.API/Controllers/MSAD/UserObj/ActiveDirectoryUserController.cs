af07e03eb5e92dce3394674e46df5c64e2ca5518
OneNotePerPage <danijel.james+onenoteperpage@gmail.com>
2024-05-22 13:47:08 +1000

    add controllers

---
+++ ActivusX.API/Controllers/MSAD/UserObj/ActiveDirectoryUserController.cs
@@ -0,0 +1,42 @@
+﻿using ActivusX.API.Data;
+using ActivusX.Shared.Models.MSAD;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ActivusX.API.Controllers.System;
+
+[Route("api/MSAD/UserObj/[controller]")]
+[ApiController]
+public class ActiveDirectoryUserController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public ActiveDirectoryUserController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: All AD users    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ActiveDirectoryUser>>> GetUsers()
+    {
+        return await _context.ActiveDirectoryUsers.ToListAsync();
+    }
+
+    // GET: Count number of AD users
+    [HttpGet]
+    public async Task<ActionResult<int>> GetUsersCount()
+    {
+        var count = await _context.ActiveDirectoryUsers.ToListAsync();
+        return count.Count;
+    }
+
+    // GET: Disabled users
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<ActiveDirectoryUser>>> GetDisabledUsers()
+    {
+        return await _context.ActiveDirectoryUsers
+            .Where(u => u.AccountEnabled == false)
+            .ToListAsync();
+    }
+}
---
+++ ActivusX.API/Controllers/System/AXUserAccountController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ActivusX.API.Data;
+using ActivusX.Shared.Models.System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ActivusX.API.Controllers.System;
+
+[Route("api/MSAD/UserObj/[controller]")]
+[ApiController]
+public class AXUserAccountController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public AXUserAccountController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AXUserAccount>>> GetUsers()
+    {
+        return await _context.AXUserAccounts.ToListAsync();
+    }
+}
